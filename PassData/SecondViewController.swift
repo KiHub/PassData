c93668d9b797e4772bebcd1ac405fa489c264434
Kirill Ki <kirillkalapov@gmail.com>
2021-06-11 13:47:05 +0200

    Finished interface

---
+++ PassData/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  PassData
+//
+//  Created by  Mr.Ki on 11.06.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBAction func gobackPressed(_ sender: Any) {
+    }
+    
+}
--- PassData/ViewController.swift
+++ PassData/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
