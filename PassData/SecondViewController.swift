120c01c239b3a5cb277ea230218280e26e718bbf
Kirill Ki <kirillkalapov@gmail.com>
2021-06-11 16:03:56 +0200

    In progress

--- PassData/SecondViewController.swift
+++ PassData/SecondViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var login: String?
+    
     @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let login = self.login else {
+            return
+        }
+        label.text = "Hello, \(login)"
+    }
+    
     @IBAction func gobackPressed(_ sender: Any) {
     }
     
--- PassData/ViewController.swift
+++ PassData/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var passLogo: UILabel!
     @IBOutlet weak var loginTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
@@ -20,8 +21,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "firstSegue", sender: nil)
     }
     
+    @IBAction func unwindSegueToMainScreen(segue: UIStoryboardSegue) {
+        
+        guard let svc = segue.source as? SecondViewController else {return}
+        self.passLogo.text = loginTF.text
+    }
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else {return}
+        dvc.login = loginTF.text
+        
+        
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
